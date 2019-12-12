67bf4aceda3496aa08308d1d08a7d81d3ab3a01e
NiallMcCann10 <34485940+NiallMcCann10@users.noreply.github.com>
2019-12-12 11:39:33 +0000

    Updated Comments

--- flaskApp.py
+++ flaskApp.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template
-
+#Creating the Flask App
 app = Flask(__name__)
-
+#Adding a route for the webpage
 @app.route("/")
 def index():
     return render_template("WebApp.html")
